240a9690189d8cbd3f97f724a7b60db06a36f86a
shahma2001 <24mcam02@meaec.edu.in>
2025-03-18 14:29:58 +0530

    Add files via upload

---
+++ p3.sh
@@ -0,0 +1,17 @@
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+echo "Enter third number: "
+read num3
+if [ $num1 -ge $num2 ] && [ $num1 -ge $num3 ]; then
+    largest=$num1
+elif [ $num2 -ge $num1 ] && [ $num2 -ge $num3 ]; then
+    largest=$num2
+else
+    largest=$num3
+fi
+echo "The largest number is: $largest"
+
+
+
